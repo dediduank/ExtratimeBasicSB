a2adac092deecb9252742dd388f119effb337383
Wardani <dediduank@gmail.com>
2021-11-11 20:20:23 +0700

    create project

---
+++ src/main/java/com/example/extratime/extratime/basic/projectController.java
@@ -0,0 +1,26 @@
+package com.example.extratime.extratime.basic;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class projectController {
+    
+    @RequestMapping("/pembagian")
+    @ResponseBody
+    public String getBagi(){
+        String rslt = "";
+
+        int mamat = 30; int puji = 10;
+        int hitung = mamat/puji;
+
+        rslt = Integer.toString(hitung);
+
+        return rslt;
+    }
+
+
+
+
+}
